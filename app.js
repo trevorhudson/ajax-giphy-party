7eaf868d146fa062b6ca3d90a940ff626a84b762
Christopher Lim <cli3338198@gmail.com>
2022-10-04 17:11:14 -0700

    made more changes

--- app.js
+++ app.js
@@ -1,53 +1,63 @@
-'use strict';
+"use strict";
 
-console.log("Let's get this party started!");
+console.log("const's get this party started!");
+
+const BASE_URL = "http://api.giphy.com/v1/gifs/search";
+const API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
 
 /** Function receives an AJAX data object linking to a gif url.
  * appends the new gif to the DOM.
  * returns undefined.
  */
-  function addGIF(gifObj){
-
-    let gif = gifObj.images.preview_gif.url;
-    $('<img class = gif>')
-      .attr('src', gif)
-      .appendTo($('.gifGallery'));
-  }
-
+function addGIF(gif) {
+  $("<img class = gif>").attr("src", gif).appendTo($(".gifGallery"));
+}
 
 /** Function handles click on 'submit' button. Queries the GIPHY API with the
  * search input. Selects a random GIF from the matched items.
  * returns a promise, and calls addGIF();
-  */
-  async function handleSubmit(e) {
-    e.preventDefault();
+ */
+async function handleSubmit(e) {
+  e.preventDefault();
 
-    let response = await axios.get(getURL());
-    let reponseLength = response.data.data.length;
-    let randomIndex = ((reponseLength) => Math.floor(Math.random() * reponseLength));
+  const {
+    data: { data },
+  } = await axios.get(BASE_URL, getURL());
 
-    addGIF(response.data.data[randomIndex(reponseLength)]);
+  const {
+    images: {
+      preview_gif: { url },
+    },
+  } = findRandomGif(data);
 
-  }
+  addGIF(url);
+}
 
+/**
+ * findRandomGif: gets a random gif
+ */
+function findRandomGif(gifs) {
+  const len = gifs.length;
+  const randomIdx = Math.floor(Math.random() * len);
+  return gifs[randomIdx];
+}
 
-  /** function generates a URL string with the search term inputs.
-   * uses giphy API
-   * returns the url as a string.
-   */
-  function getURL(){
-    let searchTerm = $('#searchInput').val();
-    let url = `http://api.giphy.com/v1/gifs/search?q=${searchTerm}` +
-      `&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
+/** function generates a URL string with the search term inputs.
+ * uses giphy API
+ * returns the url as a string.
+ */
+function getURL() {
+  const searchTerm = $("#searchInput").val();
+  return {
+    params: {
+      q: searchTerm,
+      api_key: API_KEY,
+    },
+  };
+}
 
-    return url;
-  }
+$("#inputForm").submit(handleSubmit);
 
-
-  $('#inputForm').submit(handleSubmit)
-
-  $('#removeGifs').on('click', function() {
-    $('.gifGallery').empty();
-  })
-
-
+$("#removeGifs").on("click", function () {
+  $(".gifGallery").empty();
+});
